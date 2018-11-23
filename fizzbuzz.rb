00eba6165171e87b05d62d21aa64edac772a51f4
Woody Lucas <wlucas0311@gmail.com>
2018-11-23 20:58:58 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,9 @@
 def fizzbuzz(num)
-  if (num % 3) && (num % 5)
-    return "FizzBuzz"
+  if num % 3 == 0 
+    return "Fizz"
+    elsif num % 5 == 0 
+    return "Buzz"
+    elsif (num % 3) && (num % 5)
+    return "FizzBuzz" 
  end 
 end 
